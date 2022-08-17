9349c68a3aa653231301c6a67159d71907055542
lioaslan <leoalan5577@gmail.com>
2022-08-17 21:18:21 +0700

    add: markdown preview, comment for each plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,15 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  -- Theme dracula
   use 'dracula/vim'
-  use 'wbthomason/packer.nvim' -- Package manager
+  -- Package manager
+  use 'wbthomason/packer.nvim' 
+  -- File explorer
   use 'preservim/nerdtree'
+  -- diagnostic
   use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+  -- Linting
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
@@ -14,27 +19,34 @@ return require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip'
   use 'hrsh7th/cmp-vsnip'
   use 'hrsh7th/vim-vsnip'
-
+  -- Find, grep files
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
   -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
+  -- Icon for files
   use 'kyazdani42/nvim-web-devicons'
+  -- Git manager
   use 'tpope/vim-fugitive'
+  -- Tabline
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
-
+  -- Format cod3
   use 'sbdchd/neoformat'
+  -- Some utilize short keys
   use 'tpope/vim-surround'
+  -- Buffer line
   use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
-
+  -- Comment code
   use {
       'numToStr/Comment.nvim',
       config = function()
           require('Comment').setup()
       end
   }
+  -- Preview markdown
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 end)
