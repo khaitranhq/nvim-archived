adfbcd0a85ca597a1bf72359685ef03ffbaca8a3
lioaslan <leoalan5577@gmail.com>
2022-08-22 23:46:54 +0700

    add (plugins): smoothly scroll

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -50,4 +50,7 @@ return require('packer').startup(function(use)
   }
   -- Preview markdown
   use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
+  -- Smoothly scroll
+  use 'karb94/neoscroll.nvim'
 end)
