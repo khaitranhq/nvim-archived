9f0874b226983f0bd1aca6b376a4698f530aef0f
Khai Tran <leoalan5577@gmail.com>
2022-09-01 15:42:56 +0700

    fix (global-configs): format, foldlevelstart

--- lua/global-configs.lua
+++ lua/global-configs.lua
@@ -1,44 +1,45 @@
 local opt = vim.opt
 
-opt.tabstop=2
-opt.softtabstop=0
-opt.shiftwidth=2
+opt.tabstop = 2
+opt.softtabstop = 0
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.lazyredraw = true
 
 opt.ignorecase = true
-opt.smartcase=true
+opt.smartcase = true
 
-opt.number=true
-opt.relativenumber=true
+opt.number = true
+opt.relativenumber = true
 
-opt.encoding='UTF-8'
-opt.mouse='a'
+opt.encoding = "UTF-8"
+opt.mouse = "a"
 
-opt.history=1000
-opt.undolevels=1000
-opt.clipboard='unnamed,unnamedplus'
+opt.history = 1000
+opt.undolevels = 1000
+opt.clipboard = "unnamed,unnamedplus"
 opt.autoread = true
 
-opt.spelllang='en'
+opt.spelllang = "en"
 
 -- Fold
-opt.foldmethod='syntax'
+opt.foldmethod = "syntax"
 -- TODO set foldmethod=indent for python
-opt.foldcolumn='1'
-opt.foldlevelstart=99
+vim.api.nvim_command("syntax on")
+opt.foldcolumn = "1"
+opt.foldlevelstart = 99
 
 -- Move between splitted tabs
-vim.api.nvim_set_keymap('', '<c-k>', ':wincmd k<cr>', { noremap = true})
-vim.api.nvim_set_keymap('', '<c-j>', ':wincmd j<cr>', { noremap = true})
-vim.api.nvim_set_keymap('', '<c-h>', ':wincmd h<cr>', { noremap = true})
-vim.api.nvim_set_keymap('', '<c-l>', ':wincmd l<cr>', { noremap = true})
+vim.api.nvim_set_keymap("", "<c-k>", ":wincmd k<cr>", {noremap = true})
+vim.api.nvim_set_keymap("", "<c-j>", ":wincmd j<cr>", {noremap = true})
+vim.api.nvim_set_keymap("", "<c-h>", ":wincmd h<cr>", {noremap = true})
+vim.api.nvim_set_keymap("", "<c-l>", ":wincmd l<cr>", {noremap = true})
 
-vim.api.nvim_set_keymap('', 'qq', ':qa<cr>', { noremap = true})
-vim.g.mapleader = ' '
+vim.api.nvim_set_keymap("", "qq", ":qa<cr>", {noremap = true})
+vim.g.mapleader = " "
 
-vim.api.nvim_set_keymap('n', ';', ':', { noremap = true})
+vim.api.nvim_set_keymap("n", ";", ":", {noremap = true})
 
 -- Turn on check spell
-vim.api.nvim_set_keymap('n', 'sp', ':set spell<cr>', { noremap = true})
-vim.api.nvim_set_keymap('n', 'ps', ':set spell!<cr>', { noremap = true})
+vim.api.nvim_set_keymap("n", "sp", ":set spell<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "ps", ":set spell!<cr>", {noremap = true})
